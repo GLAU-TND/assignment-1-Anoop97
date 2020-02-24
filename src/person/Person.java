a2ce22a5265e86544f6157d9db2079d4dd2ba40f
ANOOP VERMA <anoop.kumar_cs18@gla.ac.in>
2020-02-24 22:39:16 +0530

    getter setter created

--- src/person/Person.java
+++ src/person/Person.java
@@ -17,4 +17,36 @@ public class Person {
 
     public Person() {
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<String> getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(ArrayList<String> phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
