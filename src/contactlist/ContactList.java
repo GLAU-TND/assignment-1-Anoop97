dfdccd43f51b8b3cf48f60b51652634f893b01f9
ANOOP VERMA <anoop.kumar_cs18@gla.ac.in>
2020-02-24 22:19:17 +0530

    create Node class inside the contactList class

--- src/contactlist/ContactList.java
+++ src/contactlist/ContactList.java
@@ -3,6 +3,24 @@ package contactlist;
 import contactADT.ContactADT;
 
 public class ContactList<Person> implements ContactADT<Person> {
+
+    class Node<Person> {
+        Person data;
+        Node<Person> next;
+
+        private Node(Person data, Node<Person> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        private Person getData() {
+            return data;
+        }
+
+        private Node<Person> getNext() {
+            return next;
+        }
+    }
     @Override
     public boolean add(Person data) {
         return false;
