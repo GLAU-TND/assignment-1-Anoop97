2a1e44869b0ced812c87f762487236af51a2aee4
ANOOP VERMA <anoop.kumar_cs18@gla.ac.in>
2020-02-24 22:43:05 +0530

    all variables declared

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,20 @@
 package main;
 
+import contactlist.ContactList;
+import person.Person;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstName;
+        String lastName;
+        String email;
+        ContactList<Person> contactList = new ContactList<>();
+        ArrayList<String> names = new ArrayList<>();
+        char option;
+    }
 }
+
