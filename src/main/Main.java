3e28af8d08a4e48a0fc65ecc31eedeaf596bbc0c
ANOOP VERMA <anoop.kumar_cs18@gla.ac.in>
2020-02-24 22:45:16 +0530

    case 1 created to add the contact in list

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,6 +15,49 @@ public class Main {
         ContactList<Person> contactList = new ContactList<>();
         ArrayList<String> names = new ArrayList<>();
         char option;
+        while (true) {
+            System.out.println("Welcome to Abhishek's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program");
+            int choice = scanner.nextInt();
+            if (choice == 1 || choice == 2 || choice == 3 || choice == 4) {
+                switch (choice) {
+                    case 1:
+                        System.out.println("You have chosen to add a new contact: \n" +
+                                "Please enter the name of the Person\n");
+                        System.out.print("First Name: ");
+                        firstName = scanner.next();
+                        System.out.print("Last Name: ");
+                        lastName = scanner.next();
+                        names.add(firstName + " " + lastName);
+                        System.out.print("Phone Number: ");
+                        ArrayList<String> phoneNumber = new ArrayList<>();
+                        String mobileNumber = scanner.next();
+                        phoneNumber.add(mobileNumber);
+                        System.out.print("Would you like to add another contact number? (y/n): ");
+                        option = scanner.next().charAt(0);
+                        while (true) {
+                            if (option == 'y' || option == 'Y') {
+                                System.out.print("Phone Number: ");
+                                mobileNumber = scanner.next();
+                                phoneNumber.add(mobileNumber);
+                                System.out.print("Would you like to add another contact number? (y/n): ");
+                                option = scanner.next().charAt(0);
+                            } else if (option == 'n' || option == 'N') {
+                                break;
+                            }
+                        }
+                        System.out.print("Email: ");
+                        email = scanner.next();
+                        Person person = new Person(firstName, lastName, email, phoneNumber);
+                        contactList.add(person);
+                        break;
+                }
+            }
+        }
     }
 }
 
