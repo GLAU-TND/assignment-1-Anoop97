f8852af3e54da85eba0d67275788e4c53fcdd24f
ANOOP VERMA <anoop.kumar_cs18@gla.ac.in>
2020-02-24 22:38:40 +0530

    constructor created

--- src/person/Person.java
+++ src/person/Person.java
@@ -8,4 +8,13 @@ public class Person {
     String email;
     ArrayList<String> phoneNumber;
 
+    public Person(String firstName, String lastName, String email, ArrayList<String> phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Person() {
+    }
 }
